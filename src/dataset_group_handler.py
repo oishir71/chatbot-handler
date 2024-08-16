2bbcdbfc90990eda6c29aa8c227a1eac3484c88b
Reiyo <reiyo.oishi@g.softbank.co.jp>
2024-08-16 03:03:14 +0000

    Inheritance

--- src/dataset_group_handler.py
+++ src/dataset_group_handler.py
@@ -13,34 +13,44 @@ handler_format = logging.Formatter('%(asctime)s : [%(name)s - %(lineno)d] %(leve
 stream_handler.setFormatter(handler_format)
 logger.addHandler(stream_handler)
 
-class DatasetGroupHandler:
+# Handmade module
+from base_handler import BaseHandler
+
+class DatasetGroupHandler(BaseHandler):
   '''
   Chatbot用のデータセットグループを操作するためのクラス。
   '''
-  def __init__(self):
-    self.id = 'admin' # set me
-    self.password = 'password' # set me
-    self.project_id = '631a6a99-0b30-425a-bdf2-af4532ff9451' # set me
-    self.host_url = 'http://localhost:8000'
+  def __init__(
+    self,
+    host_url='http://localhost:8000',
+    id='admin',
+    password='password',
+    project_id='631a6a99-0b30-425a-bdf2-af4532ff9451',
+  ):
+    super().__init__(host_url=host_url, id=id, password=password, project_id=project_id)
+    self.base_url = '%s/api/chatbot/v1.0/projects/%s/datasetgroups' % (host_url, project_id)
+    self.authorization = (id, password)
 
   def get_datasetgroup_details(self):
     '''
     プロジェクト配下のデータセットグループを取得する
     '''
     response = requests.get(
-      '%s/api/chatbot/v1.0/projects/%s/datasetgroups/' % (self.host_url, self.project_id),
-      auth=(self.id, self.password),
+      '%s/' % (self.base_url),
+      auth=self.authorization
     )
+    self.parse_response(response=response)
     pprint.pprint(response.json())
 
   def get_datasetgroup_detail(self, datasetgroup_uuid: str):
     '''
-    hoge
+    特定のデータセットグループの詳細を取得する
     '''
     response = requests.get(
-      '%s/api/chatbot/v1.0/projects/%s/datasetgroups/%s/' % (self.host_url, self.project_id, datasetgroup_uuid),
-      auth=(self.id, self.password)
+      '%s/%s/' % (self.base_url, datasetgroup_uuid),
+      auth=self.authorization
     )
+    self.parse_response(response=response)
     pprint.pprint(response.json())
 
   def create_datasetgroup(self, name: str):
@@ -49,10 +59,12 @@ class DatasetGroupHandler:
     '''
     data={'name': name}
     response = requests.post(
-      '%s/api/chatbot/v1.0/projects/%s/datasetgroups/' % (self.host_url, self.project_id),
-      auth=(self.id, self.password),
+      '%s/' % (self.base_url),
+      headers={'Content-Type': 'application/json'},
+      auth=self.authorization,
       data=json.dumps(data)
     )
+    self.parse_response(response=response)
     pprint.pprint(response.json())
 
   def delete_datasetgroup(self, datasetgroup_uuid: str):
@@ -60,9 +72,10 @@ class DatasetGroupHandler:
     UUIDに対応するデータグループを削除する
     '''
     response = requests.delete(
-      '%s/api/chatbot/v1.0/projects/%s/datasetgroups/%s/' % (self.host_url, self.project_id, datasetgroup_uuid),
-      auth=(self.id, self.password)
+      '%s/%s/' % (self.base_url, datasetgroup_uuid),
+      auth=self.authorization
     )
+    self.parse_response(response=response)
     pprint.pprint(response.json())
 
   def get_answers(self, datasetgroup_uuid: str):
@@ -70,14 +83,16 @@ class DatasetGroupHandler:
     UUIDに対応するデータグループが持つ解答データを取得する
     '''
     response = requests.get(
-      '%s/api/chatbot/v1.0/projects/%s/datasetgroups/%s/answers/' % (self.host_url, self.project_id, datasetgroup_uuid),
-      auth=(self.id, self.password)
+      '%s/%s/answers/' % (self.base_url, datasetgroup_uuid),
+      auth=self.authorization
     )
+    self.parse_response(response=response)
     pprint.pprint(response.json())
 
 if __name__ == '__main__':
   datasetgroup_uuid = '70518ce1-5c19-46a9-8922-7c02c2462273'
 
   handler = DatasetGroupHandler()
+  handler.get_datasetgroup_details()
   handler.get_datasetgroup_detail(datasetgroup_uuid=datasetgroup_uuid)
   handler.get_answers(datasetgroup_uuid=datasetgroup_uuid)
