9abbaea22581a3e9a0f0bf37765951cade3754d6
Reiyo <reiyo.oishi@g.softbank.co.jp>
2024-08-16 06:28:01 +0000

    Cosmetics

--- src/dataset_record_handler.py
+++ src/dataset_record_handler.py
@@ -25,52 +25,53 @@ class DatasetRecordHandler(BaseHandler):
     host_url='http://localhost:8000',
     id='admin',
     password='password',
-    project_id='631a6a99-0b30-425a-bdf2-af4532ff9451'
+    project_id='631a6a99-0b30-425a-bdf2-af4532ff9451',
+    dataset_uuid='7c02c4b9-2147-4282-b7a9-db09ca934465'
   ):
     super().__init__(host_url=host_url, id=id, password=password, project_id=project_id)
-    self.base_url = '%s/api/datarepository/datasets' % (host_url)
+    self.base_url = '%s/api/datarepository/datasets/%s' % (host_url, dataset_uuid)
     self.authorization = (id, password)
 
-  def get_records(self, dataset_uuid: str):
+  def get_records(self):
     '''
     datasetに紐付けされているdataset recordのレコード一覧を取得する
     '''
     response = requests.get(
-      '%s/%s/records/' % (self.base_url, dataset_uuid),
+      '%s/records/' % (self.base_url),
       auth=self.authorization
     )
     self.parse_response(response=response)
     pprint.pprint(response.json())
 
-  def get_records_all(self, dataset_uuid: str):
+  def get_records_all(self):
     '''
     DatasetRecord内に存在する全てのレコードの一覧を取得
     '''
     response = requests.get(
-      '%s/%s/records/all/' % (self.base_url, dataset_uuid),
+      '%s/records/all/' % (self.base_url),
       auth=self.authorization
     )
     self.parse_response(response=response)
     pprint.pprint(response.json())
 
-  def get_record(self, dataset_uuid: str, record_uuid: str):
+  def get_record(self, record_uuid: str):
     '''
     record_uuidで指定されたレコードの詳細情報を取得
     '''
     response = requests.get(
-      '%s/%s/records/%s/' % (self.base_url, dataset_uuid, record_uuid),
+      '%s/records/%s/' % (self.base_url, record_uuid),
       auth=self.authorization
     )
     self.parse_response(response=response)
     pprint.pprint(response.json())
 
-  def create_record(self, dataset_uuid: str, bodies: list[dict]):
+  def create_record(self, bodies: list[dict]):
     '''
     新規レコードの追加
     '''
     data = {'data': [{'body': body} for body in bodies]}
     response = requests.post(
-      '%s/%s/records/' % (self.base_url, dataset_uuid),
+      '%s/records/' % (self.base_url),
       headers={'Content-Type': 'application/json'},
       auth=self.authorization,
       data=json.dumps(data)
@@ -78,26 +79,24 @@ class DatasetRecordHandler(BaseHandler):
     self.parse_response(response=response)
     pprint.pprint(response.json())
 
-  def delete_record(self, dataset_uuid: str, record_uuid: str):
+  def delete_record(self, record_uuid: str):
     '''
     record_uuidで指定されたレコードの削除
     '''
     response = requests.delete(
-      '%s/%s/records/%s/' % (self.base_url, dataset_uuid, record_uuid),
+      '%s/records/%s/' % (self.base_url, record_uuid),
       auth=self.authorization
     )
     self.parse_response(response=response)
 
 if __name__ == '__main__':
-  dataset_uuid='7c02c4b9-2147-4282-b7a9-db09ca934465'
   record_uuid='7025de47-6bae-48c8-b40a-48a19341df70'
 
   handler = DatasetRecordHandler()
-  handler.get_records(dataset_uuid=dataset_uuid)
-  # handler.get_records_all(dataset_uuid=dataset_uuid)
-  # handler.get_record(dataset_uuid=dataset_uuid, record_uuid=record_uuid)
+  handler.get_records()
+  # handler.get_records_all()
+  # handler.get_record(record_uuid=record_uuid)
   # handler.create_record(
-  #   dataset_uuid=dataset_uuid,
   #   bodies=[
   #     {
   #       'correct_answer': '6279c718-a990-4d8f-9dd4-b494d5d2d31e',
@@ -111,5 +110,5 @@ if __name__ == '__main__':
   #     },
   #   ]
   # )
-  handler.delete_record(dataset_uuid=dataset_uuid, record_uuid='bea1a4d2-3f13-47f3-86b5-2ed65e24f244')
-  handler.get_records(dataset_uuid=dataset_uuid)
+  # handler.delete_record(record_uuid='bea1a4d2-3f13-47f3-86b5-2ed65e24f244')
+  # handler.get_records()
